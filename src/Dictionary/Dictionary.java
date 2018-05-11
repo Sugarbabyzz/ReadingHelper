3f8590d0fb224c9d44434d12d9961edf9d3d43fd
Sugar <406857586@qq.com>
2018-05-11 09:41:36 +0800

    词典更新
    aux

--- src/Dictionary/Dictionary.java
+++ src/Dictionary/Dictionary.java
@@ -339,6 +339,23 @@ public class Dictionary {
                     buffer.append(" \n");
                 }
 
+                //aux
+                p = Pattern.compile("(.*)(<span class=\"prop\">aux.</span>)(.*?)(</p>)(.*)");
+                m = p.matcher(str2);
+                if (m.matches()) {
+                    str3 = m.group(3);
+                    buffer.append("aux. ");
+
+                    //细节
+                    p = Pattern.compile("(.*?)(<span>)(.*?)(</span>)(.*?)");
+                    m = p.matcher(str3);
+                    while (m.find()) {
+                        buffer.append(m.group(3));
+                    }
+
+                    buffer.append(" \n");
+                }
+
                 //释义
                 p = Pattern.compile("(.*)(<span class=\"prop\">释义</span>)(.*?)(</p>)(.*)");
                 m = p.matcher(str2);
